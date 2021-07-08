b0c942fe06628dc020d4a93106e62cefc305fd10
unknown <vaibhavpatil5471@gmail.com>
2021-07-08 16:32:23 +0100

    Code For Email Address

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 	public class UserRegistration {
 		public static void main(String[] args) {
 			Scanner scanner = new Scanner(System.in);
-			System.out.println("Chose Option-\n"+"1.Enter First Name\n"+"2.Enter Last Name\n");
+			System.out.println("Chose Option-\n"+"1.Enter First Name\n"+"2.Enter Last Name\n"+"3.Enter Email Address\n");
 			int option = scanner.nextInt();
 			switch (option) {
 			
@@ -39,16 +39,35 @@ import java.util.regex.Pattern;
 			
 			 String lastname = scanner2.next();
 			 
-			 string regex2 = "^[A-Z]{1}[a-z]{2}$";
+			 String regex2 = "^[A-Z]{1}[a-z]{2}$";
 			 
 			 Pattern pattern2 = Pattern.compile(regex2);
-				Matcher matcher2 = pattern.matcher(firstname);
+			 Matcher matcher2 = pattern2.matcher(lastname);
 				
-				boolean result2 = matcher2.matches();
-				if (result)
+			 boolean result2 = matcher2.matches();
+			 if (result2)
 					System.out.println("Valid lastname ");
 				else
 					System.out.println("Invali lastname");
+				
+	       case 3:
+		      Scanner scanner3 = new Scanner(System.in);
+	          System.out.println("Enter Email Address");
+
+	
+	          String email = scanner3.next();
+	 
+	          String regex3 = "^[a-zA-Z]+([.][A-Za-z]+)*@[a-zA-Z]+[.]+[a-zA-Z]{2,3}+([.][A-Za-z]+)*$";
+	 
+	          Pattern pattern3 = Pattern.compile(regex3);
+		      Matcher matcher3 = pattern3.matcher(email);
+		
+		      boolean result3 = matcher3.matches();
+		      if (result3)
+			       System.out.println("Valid Email Address ");
+		      else
+		 	       System.out.println("Invali Email Address");
+		
           
 			 
 			 
